e26fcf681e655b52ed4ef9c5015a5828a4bd7fbd
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-07-16 13:56:02 +0300

    feat: get burgers from api, send form data on api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -38,106 +39,13 @@ export class AppComponent {
     phone: ['', Validators.required],
   })
 
-  productsData = [
-    {
-      image: '1.png',
-      title: 'Бургер чеддер & бекон',
-      text: 'Котлета из говядины криспи, булочка, томат, сыр Чеддер, грудинка, лук красный, салат айсбер, майонез, кетчуп, сырный соус',
-      price: 8,
-      basePrice: 8,
-      weight: 360,
-    },
-    {
-      image: '2.png',
-      title: 'BBQ с беконом и курицей',
-      text: 'Булочка бриошь с кунжутом, куриная котлета, сыр чеддер, томат, огурец маринованный, лук маринованный, салат Ромен, бекон, соус BBQ',
-      price: 7,
-      basePrice: 7,
-      weight: 390,
-    },
-    {
-      image: '3.png',
-      title: 'Дабл биф бургер',
-      text: 'Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы, свежий томат, бекон, красный лук, соус бургер, горчица',
-      price: 10,
-      basePrice: 10,
-      weight: 420,
-    },
-    {
-      image: '4.png',
-      title: 'Баварский бургер',
-      text: 'Булочка для бургера, говяжья котлета, красный лук, сыр, охотничья колбаска, соус барбекю, соус сырный, салат айсберг',
-      price: 7,
-      basePrice: 7,
-      weight: 220,
-    },
-    {
-      image: '5.png',
-      title: 'Бекон чизбургер',
-      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, сыр, сырный соус, кетчуп, зелень',
-      price: 8,
-      basePrice: 8,
-      weight: 220,
-    },
-    {
-      image: '6.png',
-      title: 'Индиана бургер',
-      text: 'Булочка для бургера, котлета куриная, грудинка, яйцо, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
-      price: 9,
-      basePrice: 9,
-      weight: 320,
-    },
-    {
-      image: '7.png',
-      title: 'Вегги бургер',
-      text: 'Булочка для бургера, вегетарианская котлета, красный лук, сыр, свежий томат, соус барбекю, соус сырный, салат айсберг',
-      price: 8,
-      basePrice: 8,
-      weight: 280,
-    },
-    {
-      image: '8.png',
-      title: 'Плаксивый Джо',
-      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень',
-      price: 7,
-      basePrice: 7,
-      weight: 380,
-    },
-    {
-      image: '9.png',
-      title: 'Двойной чиз бургер',
-      text: 'Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
-      price: 11,
-      basePrice: 11,
-      weight: 400,
-    },
-    {
-      image: '10.png',
-      title: 'Фрешбургер',
-      text: 'Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо, салями, соус барбекю, соус сырный, салат айсберг, свежий томат',
-      price: 9,
-      basePrice: 9,
-      weight: 300,
-    },
-    {
-      image: '11.png',
-      title: 'Цуккини бургер',
-      text: 'Булочка для бургера, вегетарианская котлета из нута, цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень',
-      price: 8,
-      basePrice: 8,
-      weight: 320,
-    },
-    {
-      image: '12.png',
-      title: 'Двойной бургер чеддар',
-      text: 'Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный, томат, кетчуп, двойной сыр чеддар, горчица, зелень',
-      price: 9,
-      basePrice: 9,
-      weight: 360,
-    },
-  ];
+  productsData: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private appSevice: AppService) {
+  }
+
+  ngOnInit() {
+    this.appSevice.getData().subscribe(data => this.productsData = data);
   }
 
   scrollTo(target: HTMLElement, product?: any) {
@@ -149,7 +57,16 @@ export class AppComponent {
 
   confirmOrder() {
     if (this.form.valid) {
-      alert('Спасибо за заказ! Мы скоро свяжемся с вами!');
+      this.appSevice.sendOrder(this.form.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message)
+          },
+          error: (response) => {
+            alert(response.error.message)
+          },
+        });
+
       this.form.reset();
     }
   }
@@ -174,7 +91,7 @@ export class AppComponent {
 
     this.currency = newCurrency;
 
-    this.productsData.forEach(product => {
+    this.productsData.forEach((product: any) => {
       const totalPrice = product.basePrice * coefficient;
       product.price = Number.isInteger(totalPrice) ? totalPrice : +totalPrice.toFixed(1);
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
